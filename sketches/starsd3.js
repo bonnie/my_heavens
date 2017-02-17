3125d3f7a452db4afa021c482789e3af0e7e8580
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-02-17 12:42:43 -0800

    d3 worked out with zoe to draw stars

---
+++ sketches/starsd3.js
@@ -0,0 +1,33 @@
+d3.json('/static/data.json', printStarData);
+
+function printStarData(starData) {
+  // d3 code
+  console.log(starData);
+
+    var stars = svgContainer.selectAll("circle")
+                            .data(starData)
+                            .enter()
+                            .append('circle');
+
+
+    var starAttributes = stars
+                        .attr('cx', function(d) {return d.x})
+                        .attr('cy', function(d) {return d.y})
+                        .attr('r', function(d) {return 5-d.magnitude})
+}
+
+
+var svgBodySelection = d3.select("body");
+
+var svgContainer = svgBodySelection.append("svg")
+                               .attr("width", 400)
+                                .attr("height", 600);
+
+// var circleSelection = svgSelection.append("circle")
+//                                   .attr("cx", 25)
+//                                   .attr("cy", 25)
+//                                   .attr("r", 25)
+//                                   .style("fill", "purple");
+
+console.log('hiya!');
+

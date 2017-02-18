3c1b02d4f5cb9c372e76801c2c382f93c44b344c
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-02-17 16:51:52 -0800

    star field appears; not centered correctly (polaris is on the northern horizon!)

--- static/js/stars.js
+++ static/js/stars.js
@@ -1,8 +1,28 @@
 
-
 // get star data and display it
-d3.json('/stars.json', function(data) {
+d3.json('/stars.json', printStarData);
 
-    console.log(data);
+function printStarData(starData) {
 
-});
+    var svgBodySelection = d3.select("body");
+
+    // make a place to draw the stars
+    var svgContainer = svgBodySelection.append("svg")
+                                    .attr("width", starData.radius)
+                                    .attr("height", starData.radius);
+
+    // add the stars
+    var stars = svgContainer.selectAll("circle")
+                            .data(starData.stars)
+                            .enter()
+                            .append('circle');
+
+    var starAttributes = stars
+                        .attr('cx', function(d) {return d.x})
+                        .attr('cy', function(d) {return d.y})
+                        .attr('r', function(d) {return 4 - d.magnitude})
+                        .attr('fill', 'black')
+}
+
+
+

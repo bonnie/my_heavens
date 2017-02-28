47bf41f641e2ce00de7072cc1933ffe71671a962
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-02-28 10:15:49 -0800

    set app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] to False to avoid warnings about deprecation

--- model.py
+++ model.py
@@ -253,6 +253,7 @@ def connect_to_db(app, db_uri='postgresql:///star_chart'):
 
     # Configure to use our PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #    app.config['SQLALCHEMY_ECHO'] = True
     db.app = app
     db.init_app(app)

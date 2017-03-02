c871c12e85867c92a17e3b63c75b352db2d93f07
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-03-01 21:21:05 -0800

    added get_star_coords for SF/Johannesburg, Alpha Tel/Rigel

--- tests/calculation_tests.py
+++ tests/calculation_tests.py
@@ -20,10 +20,23 @@ TEST_DATETIME = datetime(2017, 3, 2, 5, 0, 0)
 # test lat/lngs: johannesburg
 J_LAT = '26.2041dS'
 J_LNG = '28.0473dE'
+J_LAT_RAD = -0.45734782252184614
+J_LNG_RAD = 0.4895177312946056
 
 # test lat/lngs: sf
 SF_LAT = '37.7749dN'
 SF_LNG = '122.4194dW'
+SF_LAT_RAD = 0.659296379611606
+SF_LNG_RAD = 4.14656370886364
+
+# Rigel
+R_RA = 1.372
+R_DEC = -0.143
+
+# Alpha Tel
+AT_RA = 4.851
+AT_DEC = -0.801
+
 
 class CalculationTestsWithoutDb(TestCase):  
     """Test calculations to retrieve star and constellation data.
@@ -53,10 +66,16 @@ class CalculationTestsWithoutDb(TestCase):
         self.assertEqual(lng_rad, expected_lng_rad)
 
 
-    # def star_coords_test(self)
+    def star_coords_test(self, lat, lng, ra, dec, 
+                         expected_x, expected_y, expected_visible,
+                         calculate_invisible):
 
-    #     x, y, visible = calc.get_star_coords()
+        out = calc.get_star_coords(lat, lng, TEST_DATETIME, ra, dec, TEST_RADIUS,
+                                   calculate_invisible=calculate_invisible)
 
+        self.assertEqual(out['x'], expected_x)
+        self.assertEqual(out['y'], expected_y)
+        self.assertEqual(out['visible'], expected_visible)
 
 
     ################
@@ -102,8 +121,9 @@ class CalculationTestsWithoutDb(TestCase):
         # san francisco
         self.rad_translation_test(lat=SF_LAT,
                                   lng=SF_LNG,
-                                  expected_lat_rad=0.659296379611606,
-                                  expected_lng_rad=4.14656370886364)
+                                  expected_lat_rad=SF_LAT_RAD,
+                                  expected_lng_rad=SF_LNG_RAD)
+
 
     def test_latlng_rad_Johannesburg(self):
         """Test translating latitude (south) and longitude (east) to radians.
@@ -112,9 +132,88 @@ class CalculationTestsWithoutDb(TestCase):
         # johannesburg
         self.rad_translation_test(lat=J_LAT,
                                   lng=J_LNG,
-                                  expected_lat_rad=-0.45734782252184614,
-                                  expected_lng_rad=0.4895177312946056)
+                                  expected_lat_rad=J_LAT_RAD,
+                                  expected_lng_rad=J_LNG_RAD)
 
+
+    def test_rigel_sf_position(self):
+        """Test calculation of x, y, visible for rigel in SF at TEST_DATETIME."""
+
+        # expected outcomes
+        x = 141.68829194649732
+        y = 146.0941378579376
+        vis = True
+
+        self.star_coords_test(SF_LAT_RAD, SF_LNG_RAD, R_RA, R_DEC, x, y, vis, 
+                              calculate_invisible=True)
+
+
+    def test_altel_sf_position_calculate_invisible(self):
+        """Test calculation of x, y, visible for Alpha Tel in SF at TEST_DATETIME.
+
+        Calculate coords even though it's invisible."""
+
+        # expected outcomes
+        x = -39.225014668891333
+        y = 221.53911897280611
+        vis = False
+
+        self.star_coords_test(SF_LAT_RAD, SF_LNG_RAD, AT_RA, AT_DEC, x, y, vis,
+                              calculate_invisible=True)
+
+
+    def test_altel_sf_position_skip_invisible_calculation(self):
+        """Test calculation of x, y, visible for Alpha Tel in SF at TEST_DATETIME.
+
+        Only calculate coords if visible."""
+
+        # expected outcomes
+        x = None
+        y = None
+        vis = False
+
+        self.star_coords_test(SF_LAT_RAD, SF_LNG_RAD, AT_RA, AT_DEC, x, y, vis,
+                              calculate_invisible=False)
+
+
+    def test_altel_johannesburg_position(self):
+        """Test calculation of x, y, visible for Alpha Tel in Johannesburg at TEST_DATETIME."""
+
+        # expected outcomes
+        x = 88.44111905561941
+        y = 122.69134380242504
+        vis = True
+
+        self.star_coords_test(J_LAT_RAD, J_LNG_RAD, AT_RA, AT_DEC, x, y, vis,
+                              calculate_invisible=True)
+
+
+    def test_rigel_johannesburg_position_calculate_invisible(self):
+        """Test calculation of x, y, visible for Rigel in Johannesburg at TEST_DATETIME.
+
+        Calculate coords even though it's invisible."""
+
+        # expected outcomes
+        x = 77.386186324330623
+        y = 259.86531235645788
+        vis = False
+
+        self.star_coords_test(J_LAT_RAD, J_LNG_RAD, R_RA, R_DEC, x, y, vis,
+                              calculate_invisible=True)
+
+
+    def test_rigel_johannesburg_position_skip_invisible_calculation(self):
+        """Test calculation of x, y, visible for Rigel in Johannesburg at TEST_DATETIME.
+
+        Only calculate coords if visible."""
+
+        # expected outcomes
+        x = None
+        y = None
+        vis = False
+
+        self.star_coords_test(J_LAT_RAD, J_LNG_RAD, R_RA, R_DEC, x, y, vis,
+                              calculate_invisible=False)
 
 
 class CalculationTestsWithDb(DbTestCase):  

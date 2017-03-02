f4f9c3386888551808b5d5e5a0a09c9e9fb925a8
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-03-01 19:57:30 -0800

    added tests for get_latlng_rads

--- tests/calculation_tests.py
+++ tests/calculation_tests.py
@@ -2,6 +2,7 @@
 
 from unittest import TestCase
 import math
+from datetime import datetime
 
 # be able to import from parent dir
 import sys
@@ -13,12 +14,26 @@ from run_tests import DbTestCase
 
 TEST_RADIUS = 100
 
+# 9pm PST on March 1, 2017 (which is 5am on March 2 in utc)
+TEST_DATETIME = datetime(2017, 3, 2, 5, 0, 0)
+
+# test lat/lngs: johannesburg
+J_LAT = '26.2041dS'
+J_LNG = '28.0473dE'
+
+# test lat/lngs: sf
+SF_LAT = '37.7749dN'
+SF_LNG = '122.4194dW'
 
 class CalculationTestsWithoutDb(TestCase):  
     """Test calculations to retrieve star and constellation data.
 
     This class is for tests that do not require the database.
     """
+
+    ################################
+    # Generic tests for repetition #
+    ################################
 
     def pol2cart_test(self, rho, phi, expected_x, expected_y):
         """generalized pol2cart test to avoid repeated code"""
@@ -28,6 +43,25 @@ class CalculationTestsWithoutDb(TestCase):
         self.assertEqual(x, expected_x)
         self.assertEqual(y, expected_y)
 
+
+    def rad_translation_test(self, lat, lng, expected_lat_rad, expected_lng_rad):
+        """generalized test for translating lat and lng to radians"""
+
+        lat_rad, lng_rad = calc.get_latlng_rads(lat, lng)
+
+        self.assertEqual(lat_rad, expected_lat_rad)
+        self.assertEqual(lng_rad, expected_lng_rad)
+
+
+    # def star_coords_test(self)
+
+    #     x, y, visible = calc.get_star_coords()
+
+
+
+    ################
+    # Test methods #
+    ################
 
     def test_pol2cart_north_horizon(self):
         """Test polar to cartesian coordinates, for a point on northern horizon.
@@ -59,6 +93,28 @@ class CalculationTestsWithoutDb(TestCase):
                            phi=-math.pi / 2,
                            expected_x=TEST_RADIUS * 3/2,
                            expected_y=TEST_RADIUS)
+
+
+    def test_latlng_rad_SF(self):
+        """Test translating latitude (north) and longitude (west) to radians.
+        """
+
+        # san francisco
+        self.rad_translation_test(lat=SF_LAT,
+                                  lng=SF_LNG,
+                                  expected_lat_rad=0.659296379611606,
+                                  expected_lng_rad=4.14656370886364)
+
+    def test_latlng_rad_Johannesburg(self):
+        """Test translating latitude (south) and longitude (east) to radians.
+        """
+
+        # johannesburg
+        self.rad_translation_test(lat=J_LAT,
+                                  lng=J_LNG,
+                                  expected_lat_rad=-0.45734782252184614,
+                                  expected_lng_rad=0.4895177312946056)
+
 
 
 class CalculationTestsWithDb(DbTestCase):  

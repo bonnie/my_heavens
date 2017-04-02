1b544f73e6a1560c07d0867c5d25fc03b7b9083a
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-04-02 13:41:37 -0700

    styling tweaks for side nav

--- static/js/sidenav.js
+++ static/js/sidenav.js
@@ -1,7 +1,7 @@
 // adapted from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
 
 function openNav() {
-    document.getElementById("main-sidenav").style.width = "250px";
+    document.getElementById("main-sidenav").style.width = "150px";
 }
 
 function closeNav() {

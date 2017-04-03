3f9be787cd5a072674699b1e3bbf0aac750ee518
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-04-02 21:49:21 -0700

    basic navigation in place between four 'tabs'

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def display_chart():
 
     Stars will be filled in with js"""
 
-    return render_template("stars.html", google_key=GOOGLE_KEY)
+    return render_template("main.html", google_key=GOOGLE_KEY)
 
 @app.route('/terms.json')
 def return_terms():
--- static/js/sidenav.js
+++ static/js/sidenav.js
@@ -1,5 +1,26 @@
-// adapted from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
+// functions for navigation
 
+// event listener
+$('.tab-link').on('click', function() {
+    closeNav();
+    switchTab($(this).html());
+});
+
+function switchTab(tab) {
+    // hide all other tabs but the requested one
+
+    // replace spaces
+    tab = tab === 'Star&nbsp;Map' ? 'star-map' : tab.toLowerCase();
+
+    // hide all tabs
+    $('.tab-div').hide()
+
+    // show requested tab
+    $('#' + tab).show()
+}
+
+// openNav and closeNav adapted from
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
 function openNav() {
     $('#main-sidenav').css('width', '150px');
     $('#main-sidenav').css('height', '150px');

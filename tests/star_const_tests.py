070b91d1f620a2bc2f45dc73f186d91ae2ba7e5d
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-04-26 11:51:18 -0700

    update star_const_tests to reflect non-starfield way of returning star and constellation data.

--- tests/star_const_tests.py
+++ tests/star_const_tests.py
@@ -18,10 +18,15 @@
     # along with My Heavens. If not, see <http://www.gnu.org/licenses/>.
 
 from run_tests import DbTestCase, MAX_MAG, COORDS_KEY_SET, SKYOBJECT_KEY_SET
-from stars import get_stars
+from stars import get_stars, get_const_line_groups, get_const_bound_verts, \
+    get_const_data, get_constellations
+from model import Constellation
 
 # expected star dict keys
 STAR_KEY_SET = SKYOBJECT_KEY_SET | set(['specClass', 'absMagnitude'])
+
+# expected constellation keys
+CONST_KEY_SET = set(['bound_verts', 'line_groups', 'code', 'name'])
 
 # expected constellations
 CONST_LIST_SET = set(['Orion', 'Monoceros', 'Telescopium'])
@@ -82,207 +87,155 @@ class ConstellationDataTests(DbTestCase):
     tearDownClass method inherited without change from DbTestCase
     """
 
-    # @classmethod
-    # def setUpClass(cls):
-    #     """Stuff to do once before running all class test methods."""
+    @classmethod
+    def setUpClass(cls):
+        """Stuff to do once before running all class test methods."""
 
-    #     super(ConstellationDataTests, cls).setUpClass()
-    #     super(ConstellationDataTests, cls).load_test_data()
-    #     cls.ori = Constellation.query.filter_by(const_code='ORI').one()
-    #     cls.tel = Constellation.query.filter_by(const_code='TEL').one()
-    #     cls.expected_const_keys = set(['bound_verts', 'line_groups', 'code', 'name'])
-
+        super(ConstellationDataTests, cls).setUpClass()
+        super(ConstellationDataTests, cls).load_test_data()
+        cls.ori = Constellation.query.filter_by(const_code='ORI').one()
+        cls.tel = Constellation.query.filter_by(const_code='TEL').one()
 
     #########################################################
     # Constellation Line Groups
     #########################################################
 
-    # def test_get_const_line_groups_types(self):
-    #     """Make sure the function is returning data in the expected formats"""
+    def test_get_const_line_groups_types(self):
+        """Make sure the function is returning data in the expected formats"""
 
-    #     line_groups = SF_STF.get_const_line_groups(self.ori)
+        line_groups = get_const_line_groups(self.ori)
 
-    #     example_group = line_groups[0]
-    #     example_vertex = example_group[0]
+        example_group = line_groups[0]
+        example_vertex = example_group[0]
 
-    #     self.assertIsInstance(line_groups, list) 
-    #     self.assertIsInstance(example_group, list) 
-    #     self.assertIsInstance(example_vertex, dict) 
-    #     self.assertEqual(set(example_vertex.keys()), COORDS_KEY_SET)
+        self.assertIsInstance(line_groups, list) 
+        self.assertIsInstance(example_group, list) 
+        self.assertIsInstance(example_vertex, list) 
+        self.assertEqual(len(example_vertex), 2)
 
+    def const_line_groups_test(self, const, expected_count):
+        """Test constellation line groups for various inputs"
+        """
 
-    # def const_line_groups_test(self, stf, const, expected_count):
-    #     """Test constellation line groups for various inputs"
-    #     """
+        line_groups = get_const_line_groups(const)
+        self.assertEqual(len(line_groups), expected_count)
 
-    #     line_groups = stf.get_const_line_groups(const)
-    #     self.assertEqual(len(line_groups), expected_count)
+    def test_const_line_groups_sf_ori(self):
+        """Test constellation line groups for multi-group"""
 
+        self.const_line_groups_test(self.ori, expected_count=4)
 
-    # def test_const_line_groups_sf_ori(self):
-    #     """Test constellation line groups for multi-group, visible"""
+    def test_const_line_groups_jo_ori(self):
+        """Test constellation line groups for single-group"""
 
-    #     self.const_line_groups_test(SF_STF, self.ori, expected_count=4)
-
-
-    # def test_const_line_groups_sf_tel(self):
-    #     """Test constellation line groups for single-group, not visible"""
-
-    #     self.const_line_groups_test(SF_STF, self.tel, expected_count=1)
-
-
-    # def test_const_line_groups_jo_ori(self):
-    #     """Test constellation line groups for multi-group, not visible"""
-
-    #     self.const_line_groups_test(J_STF, self.ori, expected_count=4)
-
-
-    # def test_const_line_groups_jo_ori(self):
-    #     """Test constellation line groups for single-group, visible"""
-
-    #     self.const_line_groups_test(J_STF, self.tel, expected_count=1)
+        self.const_line_groups_test(self.tel, expected_count=1)
 
 
     #########################################################
     # Constellation Boundaries
     #########################################################
 
-    # def test_get_const_bound_verts_types(self):
-    #     """Make sure the function is returning data in the expected formats"""
+    def test_get_const_bound_verts_types(self):
+        """Make sure the function is returning data in the expected formats"""
 
-    #     bound_verts = SF_STF.get_const_bound_verts(self.ori)
+        bound_verts = get_const_bound_verts(self.ori)
 
-    #     example_vertex = bound_verts[0]
+        example_vertex = bound_verts[0][0]
 
-    #     self.assertIsInstance(bound_verts, list)
-    #     self.assertIsInstance(example_vertex, dict) 
-    #     self.assertEqual(set(example_vertex.keys()), COORDS_KEY_SET)
+        self.assertIsInstance(bound_verts, list)
+        self.assertIsInstance(example_vertex, list)
+        self.assertEqual(len(example_vertex), 2)
 
+    def const_bound_verts_test(self, const, expected_count):
+        """Generic test for constellation boundary data."""
 
-    # def const_bound_verts_test(self, stf, const, expected_count):
-    #     """Generic test for constellation boundary data."""
+        verts = get_const_bound_verts(const)[0]
+        self.assertEqual(len(verts), expected_count)
 
-    #     verts = stf.get_const_bound_verts(const)
-    #     self.assertEqual(len(verts), expected_count)
+        # check that th last vertex is a repeat of the first
+        self.assertEqual(verts[0], verts[-1])
 
-    #     # check that th last vertex is a repeat of the first
-    #     self.assertEqual(verts[0], verts[-1])
+    def test_sf_ori_bound_verts(self):
+        """Test bound vertices for Orion."""
 
+        self.const_bound_verts_test(self.ori, 29)
 
-    # def test_sf_ori_bound_verts(self):
-    #     """Test bound vertices for Orion. Starfield doesn't matter for this test
-    #     """
+    def test_sf_tel_bound_verts(self):
+        """Test bound vertices for Tel."""
 
-    #     self.const_bound_verts_test(SF_STF, self.ori, 29)
-
-
-    # def test_sf_tel_bound_verts(self):
-    #     """Test bound vertices for Tel. Starfield doesn't matter for this test.
-    #     """
-
-    #     self.const_bound_verts_test(SF_STF, self.tel, 6)
+        self.const_bound_verts_test(self.tel, 6)
 
 
     #########################################################
     # Constellation Data
     #########################################################
 
-    # def test_get_const_data_types(self):
-    #     """Make sure the function is returning data in the expected formats"""
+    def test_get_const_data_types(self):
+        """Make sure the function is returning data in the expected formats"""
 
-    #     const_data = SF_STF.get_const_data(self.ori)
+        const_data = get_const_data(self.ori)
 
-    #     example_bound_verts = const_data['bound_verts']
-    #     example_bound_vertex = example_bound_verts[0]
-    #     example_line_groups = const_data['line_groups']
-    #     example_line_group = example_line_groups[0]
-    #     example_line_vertex = example_line_group[0]
+        example_bound_verts = const_data['bound_verts'][0]
+        example_bound_vertex = example_bound_verts[0]
+        example_line_groups = const_data['line_groups']
+        example_line_group = example_line_groups[0]
+        example_line_vertex = example_line_group[0]
 
-    #     self.assertIsInstance(const_data, dict) 
-    #     self.assertEqual(set(const_data.keys()), self.expected_const_keys)
+        self.assertIsInstance(const_data, dict) 
+        self.assertEqual(set(const_data.keys()), CONST_KEY_SET)
 
-    #     self.assertIsInstance(example_bound_verts, list) 
-    #     self.assertIsInstance(example_bound_vertex, dict) 
-    #     self.assertEqual(set(example_bound_vertex.keys()), COORDS_KEY_SET)
+        self.assertIsInstance(example_bound_verts, list) 
+        self.assertIsInstance(example_bound_vertex, list) 
+        self.assertEqual(len(example_bound_vertex), 2)
 
-    #     self.assertIsInstance(example_line_groups, list) 
-    #     self.assertIsInstance(example_line_group, list) 
-    #     self.assertIsInstance(example_line_vertex, dict) 
-    #     self.assertEqual(set(example_line_vertex.keys()), COORDS_KEY_SET)
-
-
-    # def get_const_data_test(self, stf, const, expected_type, expected_name):
-    #     """Generic function for getting constellation data"""
-
-    #     const_data = stf.get_const_data(const)
-
-    #     self.assertIsInstance(const_data, expected_type)
-
-    #     if expected_type == dict:
-    #         self.assertEqual(const_data['name'], expected_name)
+        self.assertIsInstance(example_line_groups, list) 
+        self.assertIsInstance(example_line_group, list) 
+        self.assertIsInstance(example_line_vertex, list) 
+        self.assertEqual(len(example_line_vertex), 2)
 
 
-    # def test_get_const_sf_ori(self):
-    #     """Test data returned for orion in sf."""
+    def get_const_data_test(self, const, expected_type, expected_name):
+        """Generic function for getting constellation data"""
 
-    #     self.get_const_data_test(SF_STF, self.ori, dict, 'Orion')
+        const_data = get_const_data(const)
 
+        self.assertIsInstance(const_data, expected_type)
 
-    # def test_get_const_sf_tel(self):
-    #     """Test data returned for telescopium in sf."""
+        if expected_type == dict:
+            self.assertEqual(const_data['name'], expected_name)
 
-    #     self.get_const_data_test(SF_STF, self.tel, dict, 'Telescopium')
+    def test_get_const_sf_ori(self):
+        """Test data returned for orion."""
 
+        self.get_const_data_test(self.ori, dict, 'Orion')
 
-    # def test_get_const_johannesburg_ori(self):
-    #     """Test data returned for orion in johannesburg."""
+    def test_get_const_sf_tel(self):
+        """Test data returned for telescopium."""
 
-    #     self.get_const_data_test(J_STF, self.ori, dict, 'Orion')
-
-
-    # def test_get_const_johannesburg_tel(self):
-    #     """Test data returned for telescopium in johannesburg."""
-
-    #     self.get_const_data_test(J_STF, self.tel, dict, 'Telescopium')
+        self.get_const_data_test(self.tel, dict, 'Telescopium')
 
 
     #########################################################
     # Constellation List
     #########################################################
 
-    # def test_get_consts_types(self):
-    #     """Make sure the function is returning data in the expected formats"""
+    def test_get_consts_types(self):
+        """Make sure the function is returning data in the expected formats"""
 
-    #     consts = SF_STF.get_consts()
-    #     example_const = consts[0]
+        consts = get_constellations()
+        example_const = consts[0]
 
-    #     self.assertIsInstance(consts, list) 
-    #     self.assertIsInstance(example_const, dict) 
-    #     self.assertEqual(set(example_const.keys()), self.expected_const_keys)
+        self.assertIsInstance(consts, list) 
+        self.assertIsInstance(example_const, dict) 
+        self.assertEqual(set(example_const.keys()), CONST_KEY_SET)
 
+    def test_const_list(self):
+        """Test list of returned constellations."""
 
-    # def const_list_test(self, stf):
-    #     """Generic test for returning constellation list. 
+        consts = get_constellations()
 
-    #     Output should be the same regardless of starfield constraints."""
+        # get the list of names with a set comprehension
+        const_names = set(const['name'] for const in consts)
 
-    #     consts = stf.get_consts()
-
-    #     # get the list of names with a set comprehension
-    #     const_names = set(const['name'] for const in consts)
-
-    #     self.assertEqual(len(consts), 3)
-    #     self.assertEqual(const_names, CONST_LIST_SET)
-
-
-    # def test_sf_consts(self):
-    #     """Test data returned for constellations in SF."""
-
-    #     self.const_list_test(SF_STF)
-
-
-    # def test_johannesburg_consts(self):
-    #     """Test data returned for constellations in Johannesburg."""
-
-    #     self.const_list_test(J_STF)
-
+        self.assertEqual(len(consts), 3)
+        self.assertEqual(const_names, CONST_LIST_SET)
